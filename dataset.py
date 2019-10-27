b2eebf898faf13047586b746e151763f47e0d595
v-jinpl@microsoft.com <v-jinpl@microsoft.com>
2019-10-27 15:18:15 +0800

    modify to can train two class segmentation

--- dataset.py
+++ dataset.py
@@ -31,19 +31,20 @@ class BaseDataset(torch.utils.data.Dataset):
         self.parse_input_list(odgt, **kwargs)
 
         # mean and std
-        self.normalize = transforms.Normalize(
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225])
+        self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                              std=[0.229, 0.224, 0.225])
 
     def parse_input_list(self, odgt, max_sample=-1, start_idx=-1, end_idx=-1):
         if isinstance(odgt, list):
             self.list_sample = odgt
         elif isinstance(odgt, str):
-            self.list_sample = [json.loads(x.rstrip()) for x in open(odgt, 'r')]
+            self.list_sample = [
+                json.loads(x.rstrip()) for x in open(odgt, 'r')
+            ]
 
         if max_sample > 0:
             self.list_sample = self.list_sample[0:max_sample]
-        if start_idx >= 0 and end_idx >= 0:     # divide file list
+        if start_idx >= 0 and end_idx >= 0:  # divide file list
             self.list_sample = self.list_sample[start_idx:end_idx]
 
         self.num_sample = len(self.list_sample)
@@ -59,7 +60,8 @@ class BaseDataset(torch.utils.data.Dataset):
 
     def segm_transform(self, segm):
         # to tensor, -1 to 149
-        segm = torch.from_numpy(np.array(segm)).long() - 1
+        # segm = torch.from_numpy(np.array(segm)).long() - 1
+        segm = torch.from_numpy(np.array(segm)).long() + 0
         return segm
 
     # Round x to the nearest multiple of p and x' >= x
@@ -87,9 +89,11 @@ class TrainDataset(BaseDataset):
             # get a sample record
             this_sample = self.list_sample[self.cur_idx]
             if this_sample['height'] > this_sample['width']:
-                self.batch_record_list[0].append(this_sample) # h > w, go to 1st class
+                self.batch_record_list[0].append(
+                    this_sample)  # h > w, go to 1st class
             else:
-                self.batch_record_list[1].append(this_sample) # h <= w, go to 2nd class
+                self.batch_record_list[1].append(
+                    this_sample)  # h <= w, go to 2nd class
 
             # update current sample pointer
             self.cur_idx += 1
@@ -128,7 +132,8 @@ class TrainDataset(BaseDataset):
         batch_widths = np.zeros(self.batch_per_gpu, np.int32)
         batch_heights = np.zeros(self.batch_per_gpu, np.int32)
         for i in range(self.batch_per_gpu):
-            img_height, img_width = batch_records[i]['height'], batch_records[i]['width']
+            img_height, img_width = batch_records[i]['height'], batch_records[
+                i]['width']
             this_scale = min(
                 this_short_size / min(img_height, img_width), \
                 self.imgMaxSize / max(img_height, img_width))
@@ -138,30 +143,33 @@ class TrainDataset(BaseDataset):
         # Here we must pad both input image and segmentation map to size h' and w' so that p | h' and p | w'
         batch_width = np.max(batch_widths)
         batch_height = np.max(batch_heights)
-        batch_width = int(self.round2nearest_multiple(batch_width, self.padding_constant))
-        batch_height = int(self.round2nearest_multiple(batch_height, self.padding_constant))
+        batch_width = int(
+            self.round2nearest_multiple(batch_width, self.padding_constant))
+        batch_height = int(
+            self.round2nearest_multiple(batch_height, self.padding_constant))
 
         assert self.padding_constant >= self.segm_downsampling_rate, \
             'padding constant must be equal or large than segm downsamping rate'
-        batch_images = torch.zeros(
-            self.batch_per_gpu, 3, batch_height, batch_width)
+        batch_images = torch.zeros(self.batch_per_gpu, 3, batch_height,
+                                   batch_width)
         batch_segms = torch.zeros(
-            self.batch_per_gpu,
-            batch_height // self.segm_downsampling_rate,
+            self.batch_per_gpu, batch_height // self.segm_downsampling_rate,
             batch_width // self.segm_downsampling_rate).long()
 
         for i in range(self.batch_per_gpu):
             this_record = batch_records[i]
 
             # load image and label
-            image_path = os.path.join(self.root_dataset, this_record['fpath_img'])
-            segm_path = os.path.join(self.root_dataset, this_record['fpath_segm'])
+            image_path = os.path.join(self.root_dataset,
+                                      this_record['fpath_img'])
+            segm_path = os.path.join(self.root_dataset,
+                                     this_record['fpath_segm'])
 
             img = Image.open(image_path).convert('RGB')
             segm = Image.open(segm_path)
-            assert(segm.mode == "L")
-            assert(img.size[0] == segm.size[0])
-            assert(img.size[1] == segm.size[1])
+            assert (segm.mode == "L")
+            assert (img.size[0] == segm.size[0])
+            assert (img.size[1] == segm.size[1])
 
             # random_flip
             if np.random.choice([0, 1]):
@@ -169,13 +177,19 @@ class TrainDataset(BaseDataset):
                 segm = segm.transpose(Image.FLIP_LEFT_RIGHT)
 
             # note that each sample within a mini batch has different scale param
-            img = imresize(img, (batch_widths[i], batch_heights[i]), interp='bilinear')
-            segm = imresize(segm, (batch_widths[i], batch_heights[i]), interp='nearest')
+            img = imresize(img, (batch_widths[i], batch_heights[i]),
+                           interp='bilinear')
+            segm = imresize(segm, (batch_widths[i], batch_heights[i]),
+                            interp='nearest')
 
             # further downsample seg label, need to avoid seg label misalignment
-            segm_rounded_width = self.round2nearest_multiple(segm.size[0], self.segm_downsampling_rate)
-            segm_rounded_height = self.round2nearest_multiple(segm.size[1], self.segm_downsampling_rate)
-            segm_rounded = Image.new('L', (segm_rounded_width, segm_rounded_height), 0)
+            segm_rounded_width = self.round2nearest_multiple(
+                segm.size[0], self.segm_downsampling_rate)
+            segm_rounded_height = self.round2nearest_multiple(
+                segm.size[1], self.segm_downsampling_rate)
+            segm_rounded = Image.new('L',
+                                     (segm_rounded_width, segm_rounded_height),
+                                     0)
             segm_rounded.paste(segm, (0, 0))
             segm = imresize(
                 segm_rounded,
@@ -199,7 +213,9 @@ class TrainDataset(BaseDataset):
         return output
 
     def __len__(self):
-        return int(1e10) # It's a fake length due to the trick that every loader maintains its own list
+        return int(
+            1e10
+        )  # It's a fake length due to the trick that every loader maintains its own list
         #return self.num_sampleclass
 
 
@@ -215,9 +231,9 @@ class ValDataset(BaseDataset):
         segm_path = os.path.join(self.root_dataset, this_record['fpath_segm'])
         img = Image.open(image_path).convert('RGB')
         segm = Image.open(segm_path)
-        assert(segm.mode == "L")
-        assert(img.size[0] == segm.size[0])
-        assert(img.size[1] == segm.size[1])
+        assert (segm.mode == "L")
+        assert (img.size[0] == segm.size[0])
+        assert (img.size[1] == segm.size[1])
 
         ori_width, ori_height = img.size
 
@@ -226,14 +242,18 @@ class ValDataset(BaseDataset):
             # calculate target height and width
             scale = min(this_short_size / float(min(ori_height, ori_width)),
                         self.imgMaxSize / float(max(ori_height, ori_width)))
-            target_height, target_width = int(ori_height * scale), int(ori_width * scale)
+            target_height, target_width = int(ori_height * scale), int(
+                ori_width * scale)
 
             # to avoid rounding in network
-            target_width = self.round2nearest_multiple(target_width, self.padding_constant)
-            target_height = self.round2nearest_multiple(target_height, self.padding_constant)
+            target_width = self.round2nearest_multiple(target_width,
+                                                       self.padding_constant)
+            target_height = self.round2nearest_multiple(
+                target_height, self.padding_constant)
 
             # resize images
-            img_resized = imresize(img, (target_width, target_height), interp='bilinear')
+            img_resized = imresize(img, (target_width, target_height),
+                                   interp='bilinear')
 
             # image transform, to torch float tensor 3xHxW
             img_resized = self.img_transform(img_resized)
@@ -272,14 +292,18 @@ class TestDataset(BaseDataset):
             # calculate target height and width
             scale = min(this_short_size / float(min(ori_height, ori_width)),
                         self.imgMaxSize / float(max(ori_height, ori_width)))
-            target_height, target_width = int(ori_height * scale), int(ori_width * scale)
+            target_height, target_width = int(ori_height * scale), int(
+                ori_width * scale)
 
             # to avoid rounding in network
-            target_width = self.round2nearest_multiple(target_width, self.padding_constant)
-            target_height = self.round2nearest_multiple(target_height, self.padding_constant)
+            target_width = self.round2nearest_multiple(target_width,
+                                                       self.padding_constant)
+            target_height = self.round2nearest_multiple(
+                target_height, self.padding_constant)
 
             # resize images
-            img_resized = imresize(img, (target_width, target_height), interp='bilinear')
+            img_resized = imresize(img, (target_width, target_height),
+                                   interp='bilinear')
 
             # image transform, to torch float tensor 3xHxW
             img_resized = self.img_transform(img_resized)
@@ -294,3 +318,70 @@ class TestDataset(BaseDataset):
 
     def __len__(self):
         return self.num_sample
+
+
+if __name__ == '__main__':
+    import os
+    import time
+    # import math
+    import random
+    import argparse
+    from distutils.version import LooseVersion
+    import matplotlib.pyplot as plt
+
+    from config import cfg
+    from lib.nn import UserScatteredDataParallel, user_scattered_collate, patch_replication_callback
+
+    assert LooseVersion(torch.__version__) >= LooseVersion('0.4.0'), \
+        'PyTorch>=0.4.0 is required'
+
+    parser = argparse.ArgumentParser(
+        description="PyTorch Semantic Segmentation Training")
+    parser.add_argument(
+        "--cfg",
+        default="config/face-resnet18dilated-ppm_deepsup.yaml",
+        metavar="FILE",
+        help="path to config file",
+        type=str,
+    )
+    parser.add_argument("--gpus",
+                        default="0",
+                        help="gpus to use, e.g. 0-3 or 0,1,2,3")
+    parser.add_argument(
+        "opts",
+        help="Modify config options using the command-line",
+        default=None,
+        nargs=argparse.REMAINDER,
+    )
+    args = parser.parse_args()
+
+    cfg.merge_from_file(args.cfg)
+    cfg.merge_from_list(args.opts)
+
+    cfg.TRAIN.batch_size = 1 * cfg.TRAIN.batch_size_per_gpu
+
+    cfg.TRAIN.max_iters = cfg.TRAIN.epoch_iters * cfg.TRAIN.num_epoch
+    cfg.TRAIN.running_lr_encoder = cfg.TRAIN.lr_encoder
+    cfg.TRAIN.running_lr_decoder = cfg.TRAIN.lr_decoder
+    dataset_train = TrainDataset(cfg.DATASET.root_dataset,
+                                 cfg.DATASET.list_train,
+                                 cfg.DATASET,
+                                 batch_per_gpu=cfg.TRAIN.batch_size_per_gpu)
+
+    loader_train = torch.utils.data.DataLoader(
+        dataset_train,
+        batch_size=1,  # we have modified data_parallel
+        shuffle=False,  # we do not use this param
+        collate_fn=user_scattered_collate,
+        drop_last=True,
+        pin_memory=True)
+
+    for batch_data in loader_train:
+        img = batch_data[0]['img_data']
+        label = batch_data[0]['seg_label']
+
+        plt.imshow(img[0].permute((1, 2, 0)).numpy())
+        plt.show()
+        plt.imshow(label[0].numpy())
+        plt.show()
+        print(np.max(label[0].numpy()), np.min(label[0].numpy()))
